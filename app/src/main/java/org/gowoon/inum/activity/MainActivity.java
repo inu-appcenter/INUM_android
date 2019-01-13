c694e9f78a3cd8f8148e5edf36727ed02f0384ec
GowoonJ <jgw971229@naver.com>
2019-01-14 00:39:30 +0900

    first commit

---
+++ app/src/main/java/org/gowoon/inum/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package org.gowoon.inum.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.gowoon.inum.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
