6da95256f68afb4a4a73bd0d1ac5c062d1db5d2e
GowoonJ <jgw971229@naver.com>
2019-01-28 02:01:18 +0900

    메뉴 카테고리 만들기 시작

--- app/src/main/java/org/gowoon/inum/util/Singleton.java
+++ app/src/main/java/org/gowoon/inum/util/Singleton.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Singleton {
     public static final RetrofitService retrofit = new Retrofit.Builder()
-            .baseUrl("http://117.16.231.66:7000/")
+            .baseUrl(Config.serverUrl)
             .addConverterFactory(GsonConverterFactory.create())
             .build().create(RetrofitService.class);
     public Singleton() {
