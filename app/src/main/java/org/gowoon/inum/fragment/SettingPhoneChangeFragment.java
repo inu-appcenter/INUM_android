5c56f6961e57a90df4db0fd435473fc1c7e5c620
GowoonJ <jgw971229@naver.com>
2019-03-31 15:52:28 +0900

    개인정보 설정 오류 수정

--- app/src/main/java/org/gowoon/inum/fragment/SettingPhoneChangeFragment.java
+++ app/src/main/java/org/gowoon/inum/fragment/SettingPhoneChangeFragment.java
@@ -13,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.JsonObject;
 
@@ -37,60 +38,69 @@ public class SettingPhoneChangeFragment extends Fragment {
                              Bundle savedInstanceState) {
         final ViewGroup rootview = (ViewGroup) inflater.inflate(R.layout.fragment_setting_phone_change, container, false);
 
-        tv_currentnum = rootview.findViewById(R.id.tv_setting_phonechange_current_num);
         final SharedPreferences pref = getActivity().getSharedPreferences("userinfo", Context.MODE_PRIVATE);
         id = pref.getString("userid","");
-        tv_currentnum.setText(pref.getString("usertel",""));
-        etv_newnum = rootview.findViewById(R.id.etv_setting_phonechange_newnum);
+        userTel = pref.getString("usertel","");
 
+        tv_currentnum = rootview.findViewById(R.id.tv_setting_phonechange_current_num);
+        tv_currentnum.setText(userTel);
+        etv_newnum = rootview.findViewById(R.id.etv_setting_phonechange_newnum);
         dialog = new Adapter_dialog_twobutton(getActivity(),"확인을 누르시면 새로운\n전화번호로 변경됩니다.");
 
         btn_changephone = rootview.findViewById(R.id.btn_setting_phone_change);
         btn_changephone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (etv_newnum.length() <10){
-                    rootview.findViewById(R.id.tv_setting_phonechange_noinput).setVisibility(View.VISIBLE);
-                }
-                else {
+                newTel = etv_newnum.getText().toString();
+                if (newTel.length() == 11){
                     rootview.findViewById(R.id.tv_setting_phonechange_noinput).setVisibility(View.INVISIBLE);
                     dialog.show();
+
+                    dialog.setOnOkButtonClickListener(new Adapter_dialog_twobutton.OnOkButtonClickListener() {
+                        @Override
+                        public void onClick() {
+                            Log.d("newTel", newTel);
+
+                            Singleton.retrofit.changeTel(id ,newTel).enqueue(new Callback<JsonObject>() {
+                                @Override
+                                public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                                    if(response.isSuccessful()){
+                                        assert response.body() != null;
+                                        JsonObject result = response.body();
+                                        Log.d("changeTel_test",""+result);
+                                        if(String.valueOf(result.get("ans")).equals("true")){
+                                            if(!userTel.equals(newTel)){
+                                                editor = pref.edit();
+                                                editor.putString("tel", newTel);
+                                                editor.apply();
+                                                tv_currentnum.setText(newTel);
+                                                FragmentManager fragmentManager = getActivity().getFragmentManager();
+                                                fragmentManager.beginTransaction()
+                                                        .remove(SettingPhoneChangeFragment.this).commit();
+                                                Toast.makeText(getActivity(),"전화번호 변경 성공!",Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                        else{
+                                            Toast.makeText(getActivity(),"전화번호 변경 실패",Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                }
+                                @Override
+                                public void onFailure(Call<JsonObject> call, Throwable t) {
+
+                                }
+                            });
+                        }
+                    });
+
+
+                }
+                else {
+                    rootview.findViewById(R.id.tv_setting_phonechange_noinput).setVisibility(View.VISIBLE);
                 }
             }
         });
 
-        dialog.setOnOkButtonClickListener(new Adapter_dialog_twobutton.OnOkButtonClickListener() {
-            @Override
-            public void onClick() {
-                newTel = etv_newnum.getText().toString();
-                Log.d("newTel", newTel);
-
-                Singleton.retrofit.changeTel(id ,newTel).enqueue(new Callback<JsonObject>() {
-                    @Override
-                    public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
-                        if(response.isSuccessful()){
-                            assert response.body() != null;
-                            JsonObject result = response.body();
-                            Log.d("changeTel_test",""+result);
-                            if(String.valueOf(result.get("ans")).equals("true")){
-                                if(!userTel.equals(newTel)){
-                                    editor = pref.edit();
-                                    editor.putString("tel", newTel);
-                                    editor.apply();
-                                    FragmentManager fragmentManager = getActivity().getFragmentManager();
-                                    fragmentManager.beginTransaction()
-                                            .remove(SettingPhoneChangeFragment.this).commit();
-                                }
-                            }
-                        }
-                    }
-                    @Override
-                    public void onFailure(Call<JsonObject> call, Throwable t) {
-
-                    }
-                });
-            }
-        });
 
         return rootview;
     }
--- app/src/main/java/org/gowoon/inum/fragment/SettingPwChangeFragment.java
+++ app/src/main/java/org/gowoon/inum/fragment/SettingPwChangeFragment.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.JsonObject;
 
@@ -30,10 +31,10 @@ import static android.content.Context.MODE_PRIVATE;
 
 public class SettingPwChangeFragment extends Fragment {
 
-    TextView noinput, diff, ok , same, less8;
-    EditText et_currentpw, et_newpw, et_newpwagain;
-    Boolean length,same_beforepw;
-    String id,pw,currentpw;
+    TextView noInput, diff, ok , same, less8;
+    EditText etCurrentPw, etNewPw, etNewPwAgain;
+    Boolean length,sameBefore, sameCurrent;
+    String id,pw,currentPw;
     SharedPreferences pref;
     
     @Override
@@ -42,35 +43,43 @@ public class SettingPwChangeFragment extends Fragment {
         ViewGroup rootview =(ViewGroup)inflater.inflate(R.layout.fragment_setting_pw_change, container, false);
 
         length = Boolean.FALSE;
-        same_beforepw = Boolean.FALSE;
+        sameBefore = Boolean.FALSE;
+        sameCurrent = Boolean.FALSE;
 
-
-        et_currentpw = rootview.findViewById(R.id.et_setting_pwchange_currentpw);
-        noinput =rootview.findViewById(R.id.tv_setting_pwchange_newpw_noinput);
+        etCurrentPw = rootview.findViewById(R.id.et_setting_pwchange_currentpw);
+        noInput =rootview.findViewById(R.id.tv_setting_pwchange_newpw_noinput);
         diff = rootview.findViewById(R.id.tv_setting_pwchange_newpw_diff);
         ok = rootview.findViewById(R.id.tv_setting_pwchange_newpw_ok);
         same = rootview.findViewById(R.id.tv_setting_pwchange_newpw_same);
         less8 = rootview.findViewById(R.id.tv_setting_pwchange_newpw_less8);
 
-        currentpw = et_currentpw.getText().toString().trim();
-        et_newpw =rootview.findViewById(R.id.et_setting_pwchange_newpw);
-        et_newpwagain = rootview.findViewById(R.id.et_setting_pwchange_newpw_again);
+        etNewPw = rootview.findViewById(R.id.et_setting_pwchange_newpw);
+        etNewPwAgain = rootview.findViewById(R.id.et_setting_pwchange_newpw_again);
 
         pref = getActivity().getSharedPreferences("userinfo",MODE_PRIVATE);
-        id = pref.getString("id","");
-        pw = pref.getString("pw","");
+        id = pref.getString("userid","");
+        pw = pref.getString("userpw","");
 
-        et_newpw.addTextChangedListener(new TextWatcher() {
+        etNewPw.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
                 if (s.toString().length()<8) {
-                    less8.setVisibility(View.VISIBLE);
-                    ok.setVisibility(View.INVISIBLE);
-                    length = false;
+                    if (s.toString().length()==0){
+                        noInput.setText("* 변경할 비밀번호를 입력해야 합니다.");
+                        noInput.setVisibility(View.VISIBLE);
+                    }else {
+                        noInput.setVisibility(View.INVISIBLE);
+                        less8.setVisibility(View.VISIBLE);
+                        ok.setVisibility(View.INVISIBLE);
+                        length = false;
+                    }
                 }
                 else {
                     less8.setVisibility(View.INVISIBLE);
@@ -78,83 +87,72 @@ public class SettingPwChangeFragment extends Fragment {
                     length = true;
                 }
             }
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
         });
 
-        et_newpwagain.addTextChangedListener(new TextWatcher() {
+        etNewPwAgain.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
             }
-
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 if (length){
-                    if (s.toString().equals(et_newpw.getText().toString())) {
+                    if (s.toString().equals(etNewPw.getText().toString())) {
                         same.setVisibility(View.VISIBLE);
                         diff.setVisibility(View.INVISIBLE);
-                        same_beforepw = true;
+                        sameCurrent = true;
                     } else {
                         diff.setVisibility(View.VISIBLE);
                         same.setVisibility(View.INVISIBLE);
-                        same_beforepw = false;
+                        sameCurrent = false;
                     }
                 }
             }
-
             @Override
             public void afterTextChanged(Editable s) {
-
             }
         });
+
         rootview.findViewById(R.id.btn_setting_pwchange).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    if ((same_beforepw) && (length)) {
-                        noinput.setVisibility(View.INVISIBLE);
-
-                        if ((currentpw.equals(pw))) {
-                            Adapter_dialog_twobutton dialog_twobtn = new Adapter_dialog_twobutton(getActivity(), "확인을 누르시면 새로운\n비밀번호로 변경됩니다.");
-                            dialog_twobtn.setOnOkButtonClickListener(new Adapter_dialog_twobutton.OnOkButtonClickListener() {
-                                @Override
-                                public void onClick() {
-                                    final String st_newpw = et_newpw.getText().toString();
-                                    Singleton.retrofit.changePasswd(id, pw, st_newpw).enqueue(new Callback<JsonObject>() {
-                                        @Override
-                                        public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
-                                            if (response.isSuccessful()) {
-                                                assert response.body() != null;
-                                                JsonObject result = response.body();
-                                                if (String.valueOf(result.get("ans")).equals("true")) {
-                                                    FragmentManager fragmentManager = getActivity().getFragmentManager();
-                                                    fragmentManager.beginTransaction().remove(SettingPwChangeFragment.this).commit();
-                                                }
+            @Override
+            public void onClick(View view) {
+                currentPw = etCurrentPw.getText().toString().trim();
+                if (pw.equals(currentPw)) {
+                    if (sameCurrent && length) {
+                        Adapter_dialog_twobutton dialog = new Adapter_dialog_twobutton(getActivity(), "확인을 누르시면 새로운\n 비밀번호로 변경됩니다.");
+                        dialog.show();
+                        dialog.setOnOkButtonClickListener(new Adapter_dialog_twobutton.OnOkButtonClickListener() {
+                            @Override
+                            public void onClick() {
+                                String newPw = etNewPwAgain.getText().toString().trim();
+                                Singleton.retrofit.changePasswd(id, pw, newPw).enqueue(new Callback<JsonObject>() {
+                                    @Override
+                                    public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                                        if (response.isSuccessful()) {
+                                            JsonObject result = response.body();
+                                            if (String.valueOf(result.get("ans")).equals("true")) {
+                                                FragmentManager fragmentManager = getActivity().getFragmentManager();
+                                                fragmentManager.beginTransaction().remove(SettingPwChangeFragment.this).commit();
+                                                Toast.makeText(getActivity(), "비밀번호 변경 성공!", Toast.LENGTH_SHORT).show();
+                                            } else {
+                                                Toast.makeText(getActivity(), "비밀번호 변경 실패", Toast.LENGTH_SHORT).show();
                                             }
                                         }
+                                    }
 
-                                        @Override
-                                        public void onFailure(Call<JsonObject> call, Throwable t) {
-                                        }
-                                    });
-                                }
-                            });
-                            dialog_twobtn.show();
-                        } else {
-                            Adapter_dialog_onebutton dialog_incorrect
-                                    = new Adapter_dialog_onebutton(getActivity(), "현재 비밀번호가\n일치하지 않습니다.");
-                            dialog_incorrect.show();
-                        }
+                                    @Override
+                                    public void onFailure(Call<JsonObject> call, Throwable throwable) {
+
+                                    }
+                                });
+                            }
+                        });
                     }
-                    else
-                    {
-                        noinput.setVisibility(View.VISIBLE);
-                    }
+                }else {
+                    Adapter_dialog_onebutton dialogInCorrect = new Adapter_dialog_onebutton(getActivity(), "현재 비밀번호가\n일치하지 않습니다.");
+                    dialogInCorrect.show();
                 }
-            });
+            }
+        });
         return rootview;
     }
 }
