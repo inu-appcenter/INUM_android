eb7e933137fe472b72896099306a08ba6a505002
gowoonJ <jgw971229@naver.com>
2019-07-03 01:43:53 +0900

    상품 업로드 수정 - 정보 입력 (진행중)

--- app/src/main/java/org/gowoon/inum/fragment/UploadInfoFragment.java
+++ app/src/main/java/org/gowoon/inum/fragment/UploadInfoFragment.java
@@ -2,10 +2,12 @@ package org.gowoon.inum.fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import org.gowoon.inum.R;
 
@@ -19,12 +21,20 @@ public class UploadInfoFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upload_info, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_upload_info, container, false);
+        TextView tvNext = getActivity().findViewById(R.id.tv_upload_next);
+        tvNext.setVisibility(View.VISIBLE);
+
+        return rootView;
     }
 
 }
