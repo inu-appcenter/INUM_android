2b4648c8b3c0433257a5f49d31d6c93bd7271ed8
GowoonJ <jgw971229@naver.com>
2019-07-24 05:27:10 +0900

    상품 업로드 - 미리보기 (서버 연결 X)

---
+++ app/src/main/java/org/gowoon/inum/fragment/UploadPreviewFragment.java
@@ -0,0 +1,131 @@
+package org.gowoon.inum.fragment;
+
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.JsonObject;
+
+import org.gowoon.inum.R;
+import org.gowoon.inum.activity.UploadActivity;
+import org.gowoon.inum.model.ProductOneItemResult;
+import org.gowoon.inum.util.Singleton;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static android.content.Context.MODE_PRIVATE;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class UploadPreviewFragment extends Fragment {
+    ProductOneItemResult uploadItemInfo = new ProductOneItemResult();
+    TextView tvName, tvState, tvPrice, tvPlace, tvMethod, tvStar, tvExplain, tvCategory;
+    String name, state, place, method, explain, category, userId, token;
+    Integer price;
+    List<String> imageUri = new ArrayList<>();
+
+    public UploadPreviewFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_upload_preview, container, false);
+        ((UploadActivity)getActivity()).initView("상품 등록 미리보기","등록",true);
+        TextView tvUpload = getActivity().findViewById(R.id.tv_upload_next);
+
+        getInfo();
+        initViewSet(rootView);
+
+        tvUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Singleton.retrofit.productUpload(token,imageUri,name,state,price,category,explain,method,place,userId)
+                        .enqueue(new Callback<JSONObject>() {
+                            @Override
+                            public void onResponse(Call<JSONObject> call, Response<JSONObject> response) {
+                                if (response.isSuccessful()){
+                                    try {
+                                        if (response.body().get("ans")=="true"){
+                                            Toast.makeText(getActivity(),"등록 완료",Toast.LENGTH_LONG).show();
+                                        }
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                                else {
+                                    Toast.makeText(getActivity(), "등록 실패", Toast.LENGTH_LONG).show();
+                                    Log.d("upload fail","fail..");
+                                }
+                            }
+
+                            @Override
+                            public void onFailure(Call<JSONObject> call, Throwable t) {
+                                Log.d("upload fail onFailure","fail..");
+                                t.getMessage();
+                                Log.d("err msg",t.getMessage());
+                            }
+                        });
+                Log.d("item info",name+price);
+            }
+        });
+
+        return rootView;
+    }
+
+    private void initViewSet(View root){
+        tvCategory = root.findViewById(R.id.tv_product_detail_category);
+        tvName = root.findViewById(R.id.tv_product_detail_name);
+        tvState = root.findViewById(R.id.tv_product_detail_status);
+        tvPlace = root.findViewById(R.id.tv_product_detail_dealplace);
+        tvPrice = root.findViewById(R.id.tv_product_detail_price);
+        tvMethod = root.findViewById(R.id.tv_product_detail_method);
+        tvStar = root.findViewById(R.id.tv_product_detail_current);
+        tvExplain = root.findViewById(R.id.tv_product_detail_info);
+
+        tvName.setText(name);
+        tvCategory.setText("-카테고리:" +category);
+        tvState.setText("-상품 상태:" +state);
+        tvExplain.setText(explain);
+//        tvPrice.setText(price.toString());
+        tvPlace.setText("-거래 장소:" +place);
+        tvMethod.setText("-거래 방식:" +method);
+        tvStar.setText("자세한 사항은 판매자에게 문의하세요!");
+    }
+
+    private void getInfo(){
+        SharedPreferences pref = Objects.requireNonNull(getActivity()).getSharedPreferences("userinfo",MODE_PRIVATE);
+        token = pref.getString("token","");
+        userId = pref.getString("userid","");
+
+        uploadItemInfo = ProductOneItemResult.getInstance();
+//        imageUri = uploadItemInfo.getProductImg();
+        imageUri.add(0,"sample");
+        name = uploadItemInfo.getProductName();
+        price = uploadItemInfo.getProductPrice();
+        place = uploadItemInfo.getPlace();
+        category = uploadItemInfo.getCategory();
+        method = uploadItemInfo.getMethod();
+        explain = uploadItemInfo.getProductInfo();
+        state = uploadItemInfo.getProductState();
+    }
+}
